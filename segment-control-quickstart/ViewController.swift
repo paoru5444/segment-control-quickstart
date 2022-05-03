c8e4f12d2d463cb09b01845e9ca3bfe9f6056fc0
Paulo Roberto <paulo.pereira.dev@gmail.com>
2022-05-02 21:21:05 -0300

    Initial Commit

---
+++ segment-control-quickstart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segment-control-quickstart
+//
+//  Created by Paulo Roberto on 02/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
